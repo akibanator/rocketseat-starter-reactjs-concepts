d5efdaf3a8f9f25f17922299fdcbf3fbaa554f5d
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2020-08-24 01:39:24 -0300

    chore: detailed page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Routes from './routes';
+
 import './styles.css'
 
 import Header from './components/Header';
@@ -8,7 +10,7 @@ import Main from './pages/main';
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
